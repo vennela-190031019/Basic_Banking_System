5accf1fbc96bd785a333278784362df9ced4d50c
vennela-190031019 <74001173+vennela-190031019@users.noreply.github.com>
2021-06-20 19:33:37 +0530

    Update transactionhistory.php

--- transactionhistory.php
+++ transactionhistory.php
@@ -41,7 +41,7 @@
 	<div align="center" style="top:0px">		  			
 		<table>
 			<tr>
-            <td style = "text-align:center;border:0px"> <a href="index.php" target="frame"><button class = "btn2"> Home </button></a></td>	 	
+            <td style = "text-align:center;border:0px"> <a href="home.php" target="frame"><button class = "btn2"> Home </button></a></td>	 	
 			<td style = "text-align:center;border:0px"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
 			<td style = "text-align:center;border:0px"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Amount</button></a></td>
 			<td style = "text-align:center;border:0px"><a href="transactionhistory.php" target="frame"><button class = "btn2"> Transaction History</button></a></td>
