14d46e3c636e35e440f58d85799aa900c470c474
vennela-190031019 <74001173+vennela-190031019@users.noreply.github.com>
2021-06-20 19:28:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<HTML>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+font-family:Arial, Helvetica, sans-serif;
+margin:0px;
+
+background-repeat:no-repeat;
+background-size: 100% 100%;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 90px;
+}
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+div.wrapper {
+width:1500px;
+height:50px;
+margin-left:auto;
+margin-right:auto;
+border:2px solid black;
+margin-top:20px;
+margin-bottom:20px;
+}
+span.navigation {
+color: #0000FF;
+float:right;
+}
+/* unvisited link */
+a.navigation:link {
+color:#000000; 
+text-decoration:none;
+}
+
+/* mouse over link */
+a.navigation:hover {
+color:#FF0000; 
+text-decoration:none;
+} 
+
+/* mouse over link */
+a.navigation:active {
+color:#FF0000; 
+text-decoration:none;
+}  
+span.navigationSpace {
+    padding-left: 50px;
+}
+.footer {
+   height: 55px;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: skyblue;
+   color: white;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+
+
+
+
+<body style="background-color:tomato;background-attachment: fixed;background-repeat: no-repeat;background-size: cover;">
+	
+
+
+
+	<div  style=" background-color:#00BFFF;
+   height: :50px;
+   width:100%;">
+		<table width="1330">
+            		
+			<table width="1200" align="center" >
+                <tr>
+                  <div style="background-color: skyblue">
+        <td  class="title" style="font-size: 40px; font-family: 'Brush Script MT', cursive; color:white;">Sparks Bank</td>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Amount </button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2"> Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+	
+	
+	<h1 style="text-align: center;font-family: 'Brush Script MT', cursive; font-size: 60px; color: green;"> Welcome to The Sparks Bank </h1>
+    <img src="mtransfer.jpg" alt="Paris" class="center"><br>
+
+</div>
+
+     
+         </div><br>
+
+<div class="footer">
+  <p>©2021 Made by Vennela</p>
+  <p> The Sparks Foundation </p>
+</div>
+</body>
+</html>
---
+++ transactionhistory.php
@@ -0,0 +1,76 @@
+<?php
+ 
+	session_start();
+	include 'connection.php';
+
+	$q="select * from transactions";
+	$result=mysqli_query($con,$q);
+	$row_count=mysqli_num_rows($result);
+	
+?>
+<html>
+<head>
+	<title>Transaction History</title>
+	<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+	<style>
+		table {
+		font-family: sans-serif;
+		border-collapse: collapse;
+		width: 80%;
+		text-align:center;
+		}
+
+		h1{
+		font-family: serif;
+		font-size:40px;
+		}
+		
+		td, th {
+		border: 2px solid white;
+		text-align: center;
+		padding: 8px;
+		}
+		tr:nth-child(odd) {
+		background-color: #00BFFF;
+		}
+	</style>
+</head>
+<body style="background-repeat: no-repeat;background-size: cover;background-blend-mode:hard-light; background-color: tomato; ">
+
+
+	<div align="center" style="top:0px">		  			
+		<table>
+			<tr>
+            <td style = "text-align:center;border:0px"> <a href="index.php" target="frame"><button class = "btn2"> Home </button></a></td>	 	
+			<td style = "text-align:center;border:0px"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
+			<td style = "text-align:center;border:0px"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Amount</button></a></td>
+			<td style = "text-align:center;border:0px"><a href="transactionhistory.php" target="frame"><button class = "btn2"> Transaction History</button></a></td>
+			</tr>
+		</table>
+	</div>
+
+    <h1 style="color:white;text-align: center;font-family: cursive; color:white;" >Transaction History</h1>
+    <table class="flat-table flat-table-1" align=center style="font-family: serif;color: white;">
+	<thead>
+		<th>SENDER NAME</th>
+		<th>RECEIVER NAME</th>
+		<th>AMOUNT</th>	
+	</thead>
+	<tbody>
+		<tr align = center>
+        <?php  
+			while($row=mysqli_fetch_array($result)){
+        ?>
+	<td><?php echo  $row["Sender"]; ?></td>
+	<td><?php echo  $row["Receiver"]; ?></td>
+	<td><?php echo  $row["Amount"]; ?></td>
+	<tr align = center>
+	<?php }
+	?>
+	</tr>
+	</tbody>
+	</table>
+	
+		
+</body>
+</html>
